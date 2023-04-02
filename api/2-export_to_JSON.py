b7ec4048da35831a9b390e4ac29215cee1dd608b
NexoNeko <meresidamente@hotmail.com>
2023-04-02 14:48:57 -0300

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -25,8 +25,8 @@ def get_todo_list(employee_id):
 
         for task in employee_todo.json():
             temp_dict = {"task": task.get('title'),
-                    "completed": task.get("completed"),
-                    "username": Username}
+                        "completed": task.get("completed"),
+                        "username": Username}
             temp.append(temp_dict)
 
         employee_id_dict = {employee_id: temp}
