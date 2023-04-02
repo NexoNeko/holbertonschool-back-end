3992cb3873f42de360979e60299ac360ae6087d9
NexoNeko <meresidamente@hotmail.com>
2023-04-02 10:12:13 -0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,26 +5,25 @@ import requests
 
 def get_todo_list(employee_id):
     """Gets the todo list from the employees"""
-    
-    #Get employee and todo list
-    employee = requests.get(f"https://jsonplaceholder.typicode.com/users?id={employee_id}")
-    employee_todo = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    employee = requests.get(\
+    "https://jsonplaceholder.typicode.com/users?id={}".format(employee_id))
+    employee_todo = requests.get(\
+    "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id))
 
     employee_name = None
     number_tasks_done = []
     number_tasks_total = 0
-    
-    #Get employee name
+
     for value in employee.json():
-        employee_name = value.get('name')
-    
-    #get task number and title of done tasks
+        employee_name = value.get('name')    
+
     for value in employee_todo.json():
         number_tasks_total += 1
-        if value.get('completed') == True:
+        if value.get('completed'):
             number_tasks_done.append(value.get('title'))
 
-    print(f"Employee {employee_name} is done with tasks({len(number_tasks_done)}/{number_tasks_total}):")
-    #display all remaining tasks here
+    print("Employee {} is done with tasks({}/{}):".format\
+    (employee_name, len(number_tasks_done), number_tasks_total))
+
     for i in number_tasks_done:
-        print(f"   {i}")
+        print("   {}".format(i))
